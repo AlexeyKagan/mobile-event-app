3da145b497bf77d90543afa95460c8e3293df6e1
Alexey <tiko1vg@gmail.com>
2017-04-28 23:13:21 +0300

    Update DataBaseUtils.js

--- nodeApiServer/server/utils/DataBaseUtils.js
+++ nodeApiServer/server/utils/DataBaseUtils.js
@@ -4,5 +4,5 @@ import config from '../config/config.json';
 
 export function setUpConnection() {
   mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`);
-  // mongoose.connect('mongodb://f7765499b9067a1f6fd76e1a26eeefa2:alex123123@32-1a.mongo.evennode.com:27017/f7765499b9067a1f6fd76e1a26eeefa2?replicaSet=eusbg1');
+  // mongoose.connect('mongodb://f7765499b9067a1f6fd76e1a26eeefa2:qweqe@32-1a.mongo.evennode.com:27017/f7765499b9067a1f6fd76e1a26eeefa2?replicaSet=eusbg1');
 }
